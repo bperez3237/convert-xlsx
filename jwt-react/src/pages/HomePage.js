58c7242841e276bd51fa9827c6fd5967a725c93b
brian <b.perez3237@gmail.com>
2023-03-20 18:38:15 -0400

    Create xlsx download request

--- jwt-react/src/pages/HomePage.js
+++ jwt-react/src/pages/HomePage.js
@@ -1,17 +1,29 @@
 import React from 'react';
 
 function HomePage() {
-  const [response, setResponse] = React.useState('');
 
   const handleFileUpload = (event) => {
     const uploadFile = (formData) => {
       fetch('http://127.0.0.1:8000/api/upload/', {
         method: 'POST',
         body: formData,
+        responseType: 'blob',
       })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.blob();
+      })
+      .then(blob => { 
+        const downloadUrl = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = downloadUrl;
+        a.download = 'excel_file.xlsx';
+        document.body.appendChild(a);
+        a.click();
+        document.body.removeChild(a);
+        URL.revokeObjectURL(downloadUrl);
       })
       .catch(error => {
         console.error(error);
@@ -21,7 +33,7 @@ function HomePage() {
     const file = event.target.files[0];
     const formData = new FormData();
     formData.append('file', file);
-    uploadFile(formData)
+    uploadFile(formData);
   }
 
   const handleClick = (e) => {
@@ -29,13 +41,11 @@ function HomePage() {
       const response = await fetch('http://127.0.0.1:8000/api/test/');
       const data = await response.json();
       console.log(data)
-      setResponse(data);
     }
     fetchData()
     .catch(console.error)
   }
 
-  console.log(response)
 
   return (<div>
       <h1>Home</h1>
--- server/api/views.py
+++ server/api/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from django.http import HttpResponse, HttpRequest, JsonResponse
+from django.http import HttpResponse, HttpRequest, JsonResponse, FileResponse
 from rest_framework.status import HTTP_200_OK
 import xlsxwriter
 import pandas as pd
+import io
 
 # Create your views here.
 def index(request):
@@ -14,15 +15,26 @@ def index(request):
         excel_file = request.FILES['file']
         sheet = pd.read_excel(excel_file)
         print(sheet)
+        output = io.BytesIO()
         # you may put validations here to check extension or file size
 
-        workbook = xlsxwriter.Workbook(excel_file)
+        workbook = xlsxwriter.Workbook(output)
         
         
         # getting a particular sheet by name out of many sheets
         worksheet = workbook.add_worksheet("Sheet1")
-
-        excel_data = list()
+        worksheet.write(0, 0, "Hello")
+        worksheet.write(1, 0, "Heloooo")
+        workbook.close()
+        
+        output.seek(0)
+        
+        print(output)
+        response = FileResponse(output, content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename="excel_file.xlsx"'
+        print(response)
+        
+        # excel_data = list()
         # iterating over the rows and
         # getting value from each cell in row
         # for row in worksheet.iter_rows():
@@ -31,10 +43,10 @@ def index(request):
         #         row_data.append(str(cell.value))
         #     excel_data.append(row_data)
 
-        return JsonResponse({"excel_data":excel_data})
+        return response
     else:
-        return HttpResponse("GET request")
+        return HttpResponse("wrong request or missing files")
     
 def test(request):
     print('j')
-    return HttpResponse('testing')
+    return JsonResponse({'message': 'testing'})
